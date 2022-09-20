468339732b2589a40ac4d19f2d9332c013e2f2a2
skajer <kacperrudy128@gmail.com>
2022-09-20 18:36:37 +0200

    Create zadaniapython.py

---
+++ programowanie/zadaniapython.py
@@ -0,0 +1,31 @@
+def zadania(a,b):
+
+    x = True
+
+    if a == 3:
+        return x
+    
+    if a < 0:
+        return x
+
+    if 0 > a == 10:
+        return x
+
+    if a <= -5:
+        return x
+
+    if (a+b) > 0:
+        return x
+
+    if (a-b) > 4:
+        return x 
+
+    if (a * b) > 100:
+        return x 
+
+    if a**2 <= 30:
+        return x 
+
+    if a % 2 != 0:
+        return x
+
